7d13d6781ff192c10ec0d52c7e7aeb576d6d8e01
kailaswagh9 <kailas.wagh189@gmail.com>
2024-02-03 16:35:57 +0530

    added new program

---
+++ SmallestAndSecondSmallestNumberFromArray.java
@@ -0,0 +1,26 @@
+package javaProgramsPkg;
+
+public class SmallestAndSecondSmallestNumberFromArray {
+
+	public static void main(String[] args) {
+		int[] num = { 1,1,3,3, 9,9,45, 67, 88,88, 99,99,108};
+		int small = Integer.MAX_VALUE;
+		int secSmall = Integer.MAX_VALUE;
+		
+		for(int i=0;i<num.length;i++) {
+			if(num[i]<small) {
+				secSmall = small;
+				small = num[i];
+			}
+			
+			if(num[i]>small && num[i]<secSmall) {
+				secSmall = num[i];	
+			}
+		}
+		System.out.println("Smallest- > " +small);
+		System.out.println("Second Smallest- > " + secSmall);
+		
+		
+	}
+
+}
