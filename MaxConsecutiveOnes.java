3314b186ffb7fac9697c883f8a61dc18f9384759
kailaswagh9 <80752561+kailaswagh9@users.noreply.github.com>
2024-02-21 02:35:02 +0530

    Add files via upload

---
+++ MaxConsecutiveOnes.java
@@ -0,0 +1,21 @@
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+      int count=0;
+      int maxConsecutiveOnes=0;
+      
+       int[] arr = {0,0,1,0,1,1,1,1,1,0,1,1};
+       
+       
+       for(int i=0;i<arr.length;i++){
+           if(arr[i]==1){
+               count++;
+               maxConsecutiveOnes = Math.max(count,maxConsecutiveOnes);
+           }else{
+               count=0;
+           }
+       }
+       
+       System.out.println(maxConsecutiveOnes);
+      
+    }
+}
---
+++ Pangram.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Pangram
+{ // if the string contains all a-z letters ignoring case then thats a pangram string
+//C:\Users\User.ITFY21EUDWIN307.000\Documents\Test\session\programs
+	public static void main(String[] args)	{
+		String str = "Abcdefghijklmnopqrstuvwxyz";
+		if(pangramstr(str)){
+		System.out.println("pangrams string");
+		}else{
+		System.out.println("Not pangrams string");
+		}	}
+
+	public static boolean pangramstr(String str)
+	{
+		HashSet<Character> set = new HashSet<>();
+		char[] ch = str.toCharArray();
+		for(char c:ch)
+		{
+		set.add(c);	
+		}	
+	
+		if(set.size() == 26)
+		{
+		return true;
+		}
+
+		return false;
+	}
+}
