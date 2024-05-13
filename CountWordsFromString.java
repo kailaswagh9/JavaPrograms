0ec85cc0927e04c9c0b845a2014bc61496de8990
kailaswagh9 <80752561+kailaswagh9@users.noreply.github.com>
2024-05-13 18:53:14 +0530

    Add files via upload

---
+++ CountWordsFromString.java
@@ -0,0 +1,25 @@
+package com.MyPkg;
+import java.util.*;
+public class CountWordsFromString {
+	
+	    public static void main(String[] args) {
+	        HashMap<String,Integer> hm = new HashMap<>();
+	        String str = "Kailas Ramdas Ramdas Ramdas Wagh Wagh";
+	        String[] split = str.split(" ");
+	        for(int i=0;i<split.length;i++){
+	             
+	             if(hm.containsKey(split[i])){
+	                 int count = hm.get(split[i]);
+	                 hm.put(split[i],count+1);
+	             }else{
+	                 hm.put(split[i],1);
+	             }
+	             
+	             
+	        }
+	        System.out.println(hm);
+	       
+	       
+	    }
+	}
+
